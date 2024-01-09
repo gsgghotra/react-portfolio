1f0fb2471a01498d37139e57537e22fa425533e4
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-09 21:24:26 +0000

    updated Navbar as component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import logo from '../../assets/images/Logo.png';
-import { NavLink } from 'react-router-dom';
+import Nav from '../Nav/Nav';
 
 const Header = () => {
     return(
@@ -9,23 +9,7 @@ const Header = () => {
                     <img src={logo} className="logo-image" alt="Text Logo of Gurjeet Singh" /></a>
             </div>
             <div className="d-flex justify-content-center">
-                <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-light">
-                    <div className="container-fluid d-flex justify-content-center">
-                        <div className="navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav d-flex align-items-center">
-                            <li className="nav-item">
-                                <NavLink to="/" end className="nav-link" > Home </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/work" className="nav-link" end> Work </NavLink>
-                            </li>
-                            <li className="nav-item">
-                            <NavLink to="/contactme" className="nav-link" end> Contact me </NavLink>
-                            </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Nav />
             </div>
         </header>
 )}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-expand-sm navbar-light">
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav d-flex align-items-center">
+                        {/* Links */}
+                    <li className="nav-item">
+                        <NavLink to="/" end className="nav-link" > Home </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/work" className="nav-link" end> Work </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contactme" className="nav-link" end> Contact me </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <a href="" target="blank" className="nav-link"> Resume </a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
