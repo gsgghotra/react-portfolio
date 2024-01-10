59e28583754a7180e072de0e672ae8a238d00a44
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-10 21:22:34 +0000

    imported required images in projectList

--- src/assets/js/projectsList.js
+++ src/assets/js/projectsList.js
@@ -1,5 +1,13 @@
 // Import images
-import project1 from '../images/harmony.png'
+import harmony from '../images/harmony.png'
+import harmony from '../images/harmony.png';
+import iphones from '../images/iphones.png';
+import weather from '../images/weather.png';
+import smartHome from '../images/smartHome.png';
+import workDay from '../images/workday.png';
+import codingTest from '../images/codingtest.png';
+import mobiles from '../images/mobiles.png';
+import scents from '../images/scents.png';
 
 const projects = [
     {name: "HarmonyHeal - Music App",
@@ -7,15 +15,9 @@ const projects = [
     description: "The ultimate music meditation app to enhance your well-being.",
     link: "https://gsgghotra.github.io/HarmonyHeal/",
     type: "Group Project (Bootcamp)",
-    image: project1
+    image: harmony
     },
-    {name: "HarmonyHeal - Music App",
-    tech: "HTML, CSS, Bootstrap, JavaScript, Rest API (Spotify API)",
-    description: "The ultimate music meditation app to enhance your well-being.",
-    link: "https://gsgghotra.github.io/HarmonyHeal/",
-    type: "Group Project (Bootcamp)",
-    image: project1
-    }
+
 ]
 
 export default projects;
