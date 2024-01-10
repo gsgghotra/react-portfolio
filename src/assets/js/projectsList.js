9e6e8862258cfb3690aafcbf1ca17e08d9d7d04a
Gurjeet Singh <gsgghotra@gmail.com>
2024-01-10 19:05:33 +0000

    List of project moved into separate js file

---
+++ src/assets/js/projectsList.js
@@ -0,0 +1,21 @@
+// Import images
+import project1 from '../images/harmony.png'
+
+const projects = [
+    {name: "HarmonyHeal - Music App",
+    tech: "HTML, CSS, Bootstrap, JavaScript, Rest API (Spotify API)",
+    description: "The ultimate music meditation app to enhance your well-being.",
+    link: "https://gsgghotra.github.io/HarmonyHeal/",
+    type: "Group Project (Bootcamp)",
+    image: project1
+    },
+    {name: "HarmonyHeal - Music App",
+    tech: "HTML, CSS, Bootstrap, JavaScript, Rest API (Spotify API)",
+    description: "The ultimate music meditation app to enhance your well-being.",
+    link: "https://gsgghotra.github.io/HarmonyHeal/",
+    type: "Group Project (Bootcamp)",
+    image: project1
+    }
+]
+
+export default projects;
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,22 +1,8 @@
 import Project from '../components/Project/Project';
 
-// Import images
-import project1 from '../assets/images/harmony.png';
+// List of Projects
+import projects from '../assets/js/projectsList';
 
-// Projects
-const projects = [
-    {name: "HarmonyHeal - Music App",
-    tech: "HTML, CSS, Bootstrap, JavaScript, Rest API (Spotify API)",
-    description: "The ultimate music meditation app to enhance your well-being.",
-    link: "https://gsgghotra.github.io/HarmonyHeal/",
-    type: "Group Project (Bootcamp)",
-    image: project1
-    }
-
-
-
-
-]
 const Work = () => {
     return(
         <section className="container-fluid" id="work">
